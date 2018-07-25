338473e9167bae9a77f2fa908c721992930d2b3a
PTAdrian1997 <poptifreaadrian@gmail.com>
2018-07-25 16:49:29 +0300

    - initial commit;

---
+++ src/app/request.ts
@@ -0,0 +1,9 @@
+/**These Requests are used exclusively for Upload;*/
+export class Request{
+	hash: string;
+	authorName: string;
+	documentName: string;
+	proposerName: string;
+	articleDescription: string;
+	contentLink: string;
+}
