ac1eb3852a436d4e035bbb1d36d4306896b2a823
MoldovanBalazs <moldovan.balazs@gmail.com>
2018-07-26 13:17:20 +0300

    deadline component on its way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,26 @@ import { DeadlineComponent } from './deadline/deadline.component';
 
 import { MainmenuComponent } from './mainmenu/mainmenu.component';
 import { ProfiledetailComponent } from './profiledetail/profiledetail.component';
+import {RouterModule, Routes} from "@angular/router";
 
+const routes: Routes = [
+      {path: '', redirectTo: 'webcontent', pathMatch: 'full'},
+      {path: 'showcase', loadChildren: './showcase/showcase.module#ShowcaseModule'},
+      {path: 'deadline', loadChildren: './showcase/showcase.module#ShowcaseModule'}
+  ];
 
 @NgModule({
   declarations: [
     AppComponent,
-
     DeadlineComponent,
-
     MainmenuComponent,
     ProfiledetailComponent,
-
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/curricula.ts
+++ src/app/curricula.ts
@@ -1,12 +1,16 @@
 export class Curricula {
+
   public name: string;
   public deadline: Date;
   public timeRemaining: Duration;
+
 }
 
 export class Duration{
-  public days:Number;
-  public hours:Number;
-  public minutes: Number;
-  public seconds:Number;
+
+  public days: number;
+  public hours: number;
+  public minutes: number;
+  public seconds: number;
+
 }
