17897cb3275489798503f82016768263c335bdb4
Daiana Tifenbacher <dtifenbacher@gmail.com>
2018-07-26 13:24:45 +0300

    Refactored article-list in articles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from "@angular/common/http";
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 
 import { DeadlineComponent } from './deadline/deadline.component';
 
 import { MainmenuComponent } from './mainmenu/mainmenu.component';
 import { ProfiledetailComponent } from './profiledetail/profiledetail.component';
-import { ArticlesComponent } from './articles/articles.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 
 
@@ -21,7 +20,6 @@ import { ArticleListComponent } from './article-list/article-list.component';
 
     MainmenuComponent,
     ProfiledetailComponent,
-    ArticlesComponent,
     ArticleListComponent,
 
   ],
