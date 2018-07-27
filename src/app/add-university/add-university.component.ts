3689b4978e2c3c75ed1ede3273eb735d7165ec00
PTAdrian1997 <poptifreaadrian@gmail.com>
2018-07-27 10:25:34 +0300

    Created add-university part;

--- src/app/add-university/add-university.component.ts
+++ src/app/add-university/add-university.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UniversityService } from '../university.service';
+import { University } from '../university';
 
 @Component({
   selector: 'app-add-university',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUniversityComponent implements OnInit {
 
-  constructor() { }
+  currentUniversity: University;
+  currentFaculty: string;
+
+  constructor(private universityService: UniversityService) {
+    this.currentUniversity = new University();
+  }
 
   ngOnInit() {
+  }
+
+  addUniversity(): void {
+    this.universityService.universities.push(this.currentUniversity);
+  }
+
+  existsUniversity(university: University): boolean {
+    return this.universityService.existsUniversity(university);
+  }
+
+  addFaculty(): void {
+    this.currentUniversity.faculties.push(this.currentFaculty);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from "@angular/common/http";
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 
 import { DeadlineComponent } from './deadline/deadline.component';
 
 import { MainmenuComponent } from './mainmenu/mainmenu.component';
 import { ProfiledetailComponent } from './profiledetail/profiledetail.component';
 
+/*Adrian's part:*/
+/*requests:*/
+import { RequestsComponent } from './requests/requests.component';
+import { RequestDetailComponent } from './request-detail/request-detail.component';
+/*add university:*/
+import { AddUniversityComponent } from './add-university/add-university.component';
+import { UniversityComponent } from './university/university.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
     DeadlineComponent,
-
     MainmenuComponent,
     ProfiledetailComponent,
-
+    /*Adrian's part:*/
+    RequestsComponent,
+    RequestDetailComponent,
+    AddUniversityComponent,
+    UniversityComponent
   ],
   imports: [
     BrowserModule,
