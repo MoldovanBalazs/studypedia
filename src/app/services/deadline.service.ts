97dd3459d2b5141e36c735638031e463e62f178f
MoldovanBalazs <moldovan.balazs@gmail.com>
2018-08-09 14:38:16 +0300

    modify deadline service

--- src/app/services/deadline.service.ts
+++ src/app/services/deadline.service.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 
 import { Deadline} from "../models/deadline";
 import { DEADLINES} from "../mock-data/mock-deadline";
+import {Subscription} from "rxjs/internal/Subscription";
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -14,18 +15,31 @@ const httpOptions = {
 export class DeadlineService {
 
   private curriculaUrl = '';
+  private deadlineList: Object[];
+
+  public headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
 
   constructor(
-    private http: HttpClient) { }
+    public http: HttpClient) { }
 
-  getCurricula(): Observable<Deadline[]> {
-    return of(DEADLINES);
+  getCurricula() : Observable<Deadline[]>{
+
+      const url = 'http://localhost:8080/deadlines';
+
+    return this.http.get<Deadline[]>(url, {headers: this.headers}).pipe();
   }
 
   addCurricula(curricula: Deadline): Observable<Deadline> {
     return this.http.post<Deadline>(this.curriculaUrl, curricula, httpOptions);
       /*.pipe(tap((curricula: Deadline) =>
         this.log(`added curricula w/ name = ${curricula.name}`)), catchError(this.handleError<Deadline>('addCurricula')));*/
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 
 }
