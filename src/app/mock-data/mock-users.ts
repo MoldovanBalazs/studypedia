b2f0868ab691546f37393d2be232789fe6c8ba87
Daiana Tifenbacher <dtifenbacher@gmail.com>
2018-08-13 20:58:40 +0300

    comment mock-users

--- src/app/mock-data/mock-users.ts
+++ src/app/mock-data/mock-users.ts
@@ -1,40 +1,40 @@
-import {User} from '../models/user';
-
-export const USERS: User[] = [
-  {
-    'id': 1,
-    'username': 'Gigel',
-    'password' : 'user',
-    'university': 'UBB',
-    'faculty' : 'Mate-Info',
-    'branch' : 'Info',
-    'userType' : 0
-  },
-  {
-    'id': 2,
-    'username': 'Maria',
-    'password' : 'user',
-    'university': 'UBB',
-    'faculty' : 'Mate-Info',
-    'branch' : 'Mate-Info',
-    'userType' : 0
-  },
-  {
-    'id': 3,
-    'username': 'Bob',
-    'password' : 'user',
-    'university': 'UBB',
-    'faculty' : 'Mate-Info',
-    'branch' : 'Mate',
-    'userType' : 1
-  },
-  {
-    'id': 4,
-    'username': 'Mariuca',
-    'password' : 'user',
-    'university': 'UTCN',
-    'faculty' : 'Automatica',
-    'branch' : '-',
-    'userType' : 1
-  }
-];
+// import {User} from '../models/user';
+//
+// export const USERS: User[] = [
+//   {
+//     'id': 1,
+//     'username': 'Gigel',
+//     'password' : 'user',
+//     'university': 'UBB',
+//     'faculty' : 'Mate-Info',
+//     'branch' : 'Info',
+//     'userType' : 0
+//   },
+//   {
+//     'id': 2,
+//     'username': 'Maria',
+//     'password' : 'user',
+//     'university': 'UBB',
+//     'faculty' : 'Mate-Info',
+//     'branch' : 'Mate-Info',
+//     'userType' : 0
+//   },
+//   {
+//     'id': 3,
+//     'username': 'Bob',
+//     'password' : 'user',
+//     'university': 'UBB',
+//     'faculty' : 'Mate-Info',
+//     'branch' : 'Mate',
+//     'userType' : 1
+//   },
+//   {
+//     'id': 4,
+//     'username': 'Mariuca',
+//     'password' : 'user',
+//     'university': 'UTCN',
+//     'faculty' : 'Automatica',
+//     'branch' : '-',
+//     'userType' : 1
+//   }
+// ];
